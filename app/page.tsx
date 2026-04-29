6d90e44eddfe9317ab0e3d05e5ea33b3e9405de0
Elroi_Noronha <elroinoronha2@gmail.com>
2026-04-29 10:05:29 +0530

    add fake log again

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import HomePage from "@/src/Screens/homepage";
 
 export default function Home() {
-  console.log("hello")
+  console.log("hello fake")
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
           <HomePage/>
